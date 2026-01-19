c0a62dc14e5995f31c9aa29d6fed580c8af77bb8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-20 01:03:49 +0530

    User middleware file added with code Done

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,25 @@
 export const AuthMiddleware = async (req, res, next) => {
     try {
-    
+        const token = req.cookie.token
+
+        if(!token){
+            return res.status(401).json({
+                message:"User not found",
+                success:false
+            })
+        }
+
+        const decode = await jwt.verify(token, process.env.JWT_SECRET)
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        }
+
+        req.id = decode.userId 
+
+        next()
     } catch (error) {
         res.status(500).json({ message: "Error auth Middleware", error: err.message });
 
