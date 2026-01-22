ea32e4fb9fd7940aff490b59adab45c98e72652c
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 19:33:00 +0530

    Update in user Routes file and user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,12 @@
 import express from 'express'
-import { getUser, Login, logOUt, register, updateProfile, upload } from '../Controller/user.controller.js'
-import { AuthMiddleware } from '../middleware/Auth.middleware.js'
+import { getUser, Login, logOUt, register, updateProfile, upload } from '../controller/userController'
+import { AuthMiddleware } from '../middlewares/auth.middleware.js';
+const userRouter = express.Router();
 
-const userRouter = express.Router()
+userRouter.route('/register').post(register);
+userRouter.route('/getUser').get(AuthMiddleware, getUser);
+userRouter.route('/login').post(Login);
+userRouter.route('/logout').post(AuthMiddleware, logOUt);
+userRouter.route('/updateProfile').post(AuthMiddleware, updateProfile);
 
-userRouter.route('/register').post(register)
-userRouter.route('/getUser').get(AuthMiddleware, getUser)
-userRouter.route('/login').post(Login)
-userRouter.route('/logout').post(AuthMiddleware, logOUt)
-userRouter.route('/updateProfile').post(AuthMiddleware, upload.single('profilPhoto'), updateProfile)
-
-export default userRouter
+export default userRouter;
