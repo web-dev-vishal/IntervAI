eb71e428ec8fe8c687eeca6f37d0520e875ea979
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-23 00:21:54 +0530

    Update in session routes  Done

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -5,8 +5,8 @@ import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 const sessionRouter = express.Router()
 
 sessionRouter.route('/createSession').post(AuthMiddleware, createSession);
-sessionRouter.route('/getSession').post(AuthMiddleware, getSession);
-sessionRouter.route('/getSessionById').post(AuthMiddleware, getSessionById);
+sessionRouter.route('/getSession').get(AuthMiddleware, getSession);
+sessionRouter.route('/getSessionById').get(AuthMiddleware, getSessionById);
 sessionRouter.route('/deleteSession').delete(AuthMiddleware, deleteSession);
 
 export default sessionRouter
