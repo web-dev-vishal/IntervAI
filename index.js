3a9e5fb12b74eadd2ae7a807d7d16d0f51b82fab
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 19:47:41 +0530

    Update in index  file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
-import { connectDB } from "./config/db.js"
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
 
 app.listen(PORT, () => {
     connectDB();
