73f988af1d3c127e7ba20504d09d1e7c34815230
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 23:46:38 +0530

    Session controller (delete Session ) added code Done

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,15 +1,15 @@
-export const createSession = async (req,res)=> {
+export const createSession = async (req, res) => {
     try {
-        const {role, exprience, topicsToFocus} = req.body;
+        const { role, exprience, topicsToFocus } = req.body;
         const userId = req.id
-        if(!role || !exprience || !topicsToFocus){
+        if (!role || !exprience || !topicsToFocus) {
             return res.status(401).json({
-                message:"Please Provide all the details"
+                message: "Please Provide all the details"
             })
         }
 
         const session = await Session.create({
-            user:userId,
+            user: userId,
             role,
             exprience,
             topicsToFocus
@@ -18,52 +18,67 @@ export const createSession = async (req,res)=> {
         await session.save()
 
         return res.status(201).json({
-            message:"Session created successfully"
+            message: "Session created successfully"
         })
     } catch (error) {
         res.status(500).json({ message: "Error in Create Session", error: err.message });
     }
 };
 
-export const getSession = async (req,res)=> {
+export const getSession = async (req, res) => {
     try {
         const userId = req.id
-        const session  = await Session.find({user:userId})
-        .sort({createdAt:-1})
-        .populate("questions")
-        
+        const session = await Session.find({ user: userId })
+            .sort({ createdAt: -1 })
+            .populate("questions")
+
         res.status(201).json({
             session
         })
-        
+
     } catch (error) {
         res.status(500).json({ message: "Error in Get Session", error: err.message });
     }
 };
 
-export const getSessionById = async (req,res)=> {
+export const getSessionById = async (req, res) => {
     try {
         const session = await Session.findById(req.params.id)
-        .sort({createdAt:-1})
-        .populate({
-            path:"questions",
-            options:{sort:{isPinned:-1}, createdAt:-1}
-        })
+            .sort({ createdAt: -1 })
+            .populate({
+                path: "questions",
+                options: { sort: { isPinned: -1 }, createdAt: -1 }
+            })
 
         return res.status(201).json({
             session
         })
     } catch (error) {
         res.status(500).json({ message: "Error in Get Session By Id", error: err.message });
-        
+
     }
 };
 
-export const deleteSession = async (req,res)=> {
+export const deleteSession = async (req, res) => {
     try {
-        
+        const session = await Session.findById(req.params.id)
+
+        if (!session) {
+            return res.status(401).json({
+                message: "Session not found"
+            })
+        }
+
+
+        await Question.deleteMany({ session: session._id })
+
+        await session.deleteOne()
+
+        return res.status(201).json({
+            message: "Session deleted successfully"
+        })
     } catch (error) {
-        
+
         res.status(500).json({ message: "Error Delete Session", error: err.message });
     }
 };
