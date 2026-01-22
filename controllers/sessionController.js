edf372ea5a8900c4e23255dde498859c7ee69dbb
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 23:43:32 +0530

    Session controller (get Session) added code Done

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -26,7 +26,19 @@ export const createSession = async (req,res)=> {
 };
 
 export const getSession = async (req,res)=> {
-
+    try {
+        const userId = req.id
+        const session  = await Session.find({user:userId})
+        .sort({createdAt:-1})
+        .populate("questions")
+        
+        res.status(201).json({
+            session
+        })
+        
+    } catch (error) {
+        res.status(500).json({ message: "Error in Get Session", error: err.message });
+    }
 };
 
 export const getSessionById = async (req,res)=> {
