92494d6223240e46245e25d3b7c2524ca22a1dd8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 19:43:24 +0530

    Update in user Routes file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { getUser, Login, logOUt, register, updateProfile } from '../controller/userController'
 import { AuthMiddleware } from '../middlewares/auth.middleware.js';
+import { getUser, Login, logOut, register, updateProfile } from '../controllers/userController.js';
 const userRouter = express.Router();
 
 userRouter.route('/register').post(register);
 userRouter.route('/getUser').get(AuthMiddleware, getUser);
 userRouter.route('/login').post(Login);
-userRouter.route('/logout').post(AuthMiddleware, logOUt);
+userRouter.route('/logout').post(AuthMiddleware, logOut);
 userRouter.route('/updateProfile').post(AuthMiddleware, updateProfile);
 
 export default userRouter;
