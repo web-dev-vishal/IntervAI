373d99afd87fda9dbe95eab133abcc62fd8d4cfe
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-22 23:40:57 +0530

    Session controller (create Session) added code Done

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,6 +1,25 @@
 export const createSession = async (req,res)=> {
     try {
-        
+        const {role, exprience, topicsToFocus} = req.body;
+        const userId = req.id
+        if(!role || !exprience || !topicsToFocus){
+            return res.status(401).json({
+                message:"Please Provide all the details"
+            })
+        }
+
+        const session = await Session.create({
+            user:userId,
+            role,
+            exprience,
+            topicsToFocus
+        })
+
+        await session.save()
+
+        return res.status(201).json({
+            message:"Session created successfully"
+        })
     } catch (error) {
         res.status(500).json({ message: "Error in Create Session", error: err.message });
     }
